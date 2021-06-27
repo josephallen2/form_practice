39f23ba3b4b9480a1009102ad35ba08a9ceb6503
Joseph <joseph_allen13@outlook.com>
2021-06-27 05:30:03 -0500

    first commit, added several forms in App.js

---
+++ src/App.js
@@ -0,0 +1,102 @@
+import logo from './logo.svg';
+import './App.css';
+
+/*
+
+    <input type="button">
+    <input type="checkbox">
+    <input type="color">
+    <input type="date">
+    <input type="datetime-local">
+    <input type="email">
+    <input type="file">
+    <input type="hidden">
+    <input type="image">
+    <input type="month">
+    <input type="number">
+    <input type="password">
+    <input type="radio">
+    <input type="range">
+    <input type="reset">
+    <input type="search">
+    <input type="submit">
+    <input type="tel">
+    <input type="text">
+    <input type="time">
+    <input type="url">
+    <input type="week">
+
+*/
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <label>
+          Text Field:
+          <input type="text" name="name" />
+        </label><br/><br/>
+
+        <label>
+          Button:
+          <input type="button" value="Click Me!" />
+        </label><br/><br/>
+
+        <label>
+          checkbox:
+          <input type="checkbox" />
+        </label><br/><br/>
+
+        <label>
+          Color:
+          <input type="color"  />
+        </label><br/><br/>
+
+        <label>
+          Date:
+          <input type="date" />
+        </label><br/><br/>
+
+        <label>
+          Email:
+          <input type="email" />
+        </label><br/><br/>
+
+        <label>
+          File:
+          <input type="file" />
+        </label><br/><br/>
+
+        <label>
+          Hidden:
+          <input type="hidden"/>
+        </label><br/><br/>
+
+        <label>
+          Month:
+          <input type="month"/>
+        </label><br/><br/>
+
+        <label>
+          Number:
+          <input type="number"/>
+        </label><br/><br/>
+
+        <label>
+          Password:
+          <input type="password"/>
+        </label><br/><br/>
+
+        <label>
+          Radio:
+          <input type="radio" />
+        </label><br/><br/>
+
+        <input type="submit" value="Submit" />
+      </form>
+
+    </div>
+  );
+}
+
+export default App;
